2f9f6d854f7c941141d8a5cf37a28874bfcfbab7
Tom Easterbrook <tomeasterbrook98@gmail.com>
2024-08-03 09:05:52 +0100

    Simple notification when page opened

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,5 +1,16 @@
-<div x-data="{}">
+<div x-data="{}"
+x-init="
+Notification.requestPermission().then((result)=>{
+    if(result === 'granted'){
+        console.log('Notification permission granted');
+    }
+});
+    Echo.channel('messages').listen('NewMessageEvent',(event)=>{
+        console.log(event);
+        new Notification(event.title,{body:event.message});
+    });
 
+">
 </div>
 <script>
 

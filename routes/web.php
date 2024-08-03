bd5aadec1d845072dd2431ee26cc417394bcd2a1
Tom Easterbrook <tomeasterbrook98@gmail.com>
2024-08-03 10:01:33 +0100

    Make serviceworker publicly avalible

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', \App\Livewire\Dashboard::class)->name('dashboard')
     ->middleware(['auth']);
+Route::get('/serviceworker.js', function () {
+    $assetPath = Vite::asset('resources/js/serviceworker.js');
+    $context = stream_context_create([
+        'ssl' => [
+            'verify_peer' => false,
+            'verify_peer_name' => false,
+        ],
+    ]);
+    $fileContent = file_get_contents($assetPath, false, $context);
+
+    return response($fileContent, 200, [
+        'Content-Type' => 'text/javascript',
+        'Cache-Control' => 'public, max-age=3600',
+    ]);
+})->name('serviceworker');

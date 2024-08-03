7bf1fd06497f4efcac7970b42ccf8214fb3a866a
Tom Easterbrook <tomeasterbrook98@gmail.com>
2024-08-03 11:08:54 +0100

    Add support for background notifications

--- resources/js/serviceworker.js
+++ resources/js/serviceworker.js
@@ -1,7 +1,5 @@
-import Echo from 'laravel-echo';
-
-import Pusher from 'pusher-js';
-
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/pusher/8.3.0/pusher.worker.min.js');
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/laravel-echo/1.16.1/echo.iife.min.js');
 const EchoClient = new Echo({
     broadcaster: 'reverb',
     key: import.meta.env.VITE_REVERB_APP_KEY,
@@ -10,4 +8,10 @@ const EchoClient = new Echo({
     wssPort: import.meta.env.VITE_REVERB_PORT ?? 443,
     forceTLS: (import.meta.env.VITE_REVERB_SCHEME ?? 'https') === 'https',
     enabledTransports: ['ws', 'wss'],
+});
+EchoClient.channel('messages').listen('NewMessageEvent',(event)=> {
+    console.log(event);
+    self.registration.showNotification(event.title, {
+        body: event.message,
+    });
 });
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -2,13 +2,16 @@
 x-init="
 Notification.requestPermission().then((result)=>{
     if(result === 'granted'){
-        console.log('Notification permission granted');
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register('/serviceworker.js').then(function(registration) {
+                console.log('Service Worker registered with scope:', registration.scope);
+            }).catch(function(error) {
+                console.log('Service Worker registration failed:', error);
+            });
+        }
     }
 });
-    Echo.channel('messages').listen('NewMessageEvent',(event)=>{
-        console.log(event);
-        new Notification(event.title,{body:event.message});
-    });
+
 
 ">
 </div>
